19071b2cdade6837436d5cc2c627d742d5cbe314
Louis Johnson <louisj381@gmail.com>
2017-11-23 03:05:35 -0700

    completed basic exercise

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -98,7 +98,6 @@ void UserApp1Initialize(void)
     /* The task isn't properly initialized, so shut it down and don't run */
     UserApp1_StateMachine = UserApp1SM_Error;
   }
-
 } /* end UserApp1Initialize() */
 
   
@@ -136,7 +135,34 @@ State Machine Function Definitions
 /* Wait for ??? */
 static void UserApp1SM_Idle(void)
 {
-
+if (WasButtonPressed(BUTTON0))
+{
+  ButtonAcknowledge(BUTTON0);
+  PWMAudioSetFrequency(BUZZER1, 262);
+}
+if (WasButtonPressed(BUTTON1))
+{
+  ButtonAcknowledge(BUTTON1);
+  PWMAudioSetFrequency(BUZZER1, 294);
+}
+if (WasButtonPressed(BUTTON2))
+{
+  ButtonAcknowledge(BUTTON2);
+  PWMAudioSetFrequency(BUZZER1, 330);
+}
+if (WasButtonPressed(BUTTON3))
+{
+  ButtonAcknowledge(BUTTON3);
+  PWMAudioSetFrequency(BUZZER1, 392);
+}
+if (IsButtonPressed(BUTTON0) || IsButtonPressed(BUTTON1) || IsButtonPressed(BUTTON2) || IsButtonPressed(BUTTON3))
+{
+  PWMAudioOn(BUZZER1);
+}
+else
+{
+  PWMAudioOff(BUZZER1);
+}
 } /* end UserApp1SM_Idle() */
     
 
